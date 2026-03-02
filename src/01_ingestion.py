4fd2c02203af9f3798a0dfbaafadfb916f21039f
androide47 <75287088+Androide47@users.noreply.github.com>
2026-03-02 17:41:27 -0600

    feat: ingestion

---
+++ src/01_ingestion.py
@@ -0,0 +1,24 @@
+import dlt
+from pyspark.sql.functions import *
+
+source_path = "s3://amzn-s2-androide-bucket/raw"
+
+@dtl.table(
+    name="raw_llm_data",
+    comment="Raw text data ingested from S3 for LLM training",
+    table_properties{
+        "quality": "bronze",
+        "pipelines.autoOptimize.manage": "ture"
+    }
+)
+
+def raw_llm_data():
+    return(
+        spark.readStream.format("cloudFiles")
+        .option("cloudFiles.format", "text")
+        .option("cloudFiles.schemaLocation","s3://amzn-s2-androide-bucket/schemas/bronze")
+        .option("cloudFiles.inferColumnTypes", "true")
+        .load(source_path)
+        .withColumn("ingestion_time", current_timestamp())
+        .withColumn("source_file", input_file_name())
+    )
